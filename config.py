ec8173f1ce0da7e2fdf0556389e63d02a38c015d
angeuwase <angeuwasedev@gmail.com>
2021-05-06 17:20:34 +0100

    changed smpt mail server port

--- config.py
+++ config.py
@@ -22,9 +22,12 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', default='A very terrible secret key.')
     
     MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
+    #MAIL_PORT = 465
+    #MAIL_USE_TLS = False
+    #MAIL_USE_SSL = True
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     MAIL_USERNAME = os.getenv('MAIL_USERNAME', default='')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', default='')
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_USERNAME', default='')
